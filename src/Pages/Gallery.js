0bf26d96a9a5b940872c16cf63808793deb73b8a
marsudien <minhphuoc1304@gmail.com>
2024-11-21 21:25:27 +0700

    use thumbnails in the Gallery for the better performance

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -31,24 +31,43 @@ import img7032 from "../Assets/Images/Gallery/IMG_7052.jpeg";
 import img7078 from "../Assets/Images/Gallery/IMG_7078.jpeg";
 import img9576 from "../Assets/Images/Gallery/IMG_9576.jpeg";
 
+// Import all thumbnail images
+import thumb0038 from "../Assets/Images/Gallery/thumbs/IMG_0038_thumb.jpeg";
+import thumb0682 from "../Assets/Images/Gallery/thumbs/IMG_0682_thumb.jpeg";
+import thumb0719 from "../Assets/Images/Gallery/thumbs/IMG_0719_thumb.jpeg";
+import thumb0730 from "../Assets/Images/Gallery/thumbs/IMG_0730_thumb.jpeg";
+import thumb0944 from "../Assets/Images/Gallery/thumbs/IMG_0944_thumb.jpeg";
+import thumb0950 from "../Assets/Images/Gallery/thumbs/IMG_0950_thumb.jpeg";
+import thumb0952 from "../Assets/Images/Gallery/thumbs/IMG_0952_thumb.jpeg";
+import thumb0955 from "../Assets/Images/Gallery/thumbs/IMG_0955_thumb.jpeg";
+import thumb2544 from "../Assets/Images/Gallery/thumbs/IMG_2544_thumb.jpeg";
+import thumb5074 from "../Assets/Images/Gallery/thumbs/IMG_5074_thumb.jpeg";
+import thumb5075 from "../Assets/Images/Gallery/thumbs/IMG_5075_thumb.jpeg";
+import thumb5129 from "../Assets/Images/Gallery/thumbs/IMG_5129_thumb.jpeg";
+import thumb5187 from "../Assets/Images/Gallery/thumbs/IMG_5187_thumb.jpeg";
+import thumb6599 from "../Assets/Images/Gallery/thumbs/IMG_6599_thumb.jpeg";
+import thumb7032 from "../Assets/Images/Gallery/thumbs/IMG_7052_thumb.jpeg";
+import thumb7078 from "../Assets/Images/Gallery/thumbs/IMG_7078_thumb.jpeg";
+import thumb9576 from "../Assets/Images/Gallery/thumbs/IMG_9576_thumb.jpeg";
+
 const galleryImages = [
-  { src: img9576, alt: "Hima Hair Salon" },
-  { src: img0038, alt: "Hima Hair Salon" },
-  { src: img0682, alt: "Hima Hair Salon" },
-  { src: img0719, alt: "Hima Hair Salon" },
-  { src: img0730, alt: "Hima Hair Salon" },
-  { src: img0944, alt: "Hima Hair Salon" },
-  { src: img0950, alt: "Hima Hair Salon" },
-  { src: img0952, alt: "Hima Hair Salon" },
-  { src: img0955, alt: "Hima Hair Salon" },
-  { src: img2544, alt: "Hima Hair Salon" },
-  { src: img5074, alt: "Hima Hair Salon" },
-  { src: img5075, alt: "Hima Hair Salon" },
-  { src: img5129, alt: "Hima Hair Salon" },
-  { src: img5187, alt: "Hima Hair Salon" },
-  { src: img6599, alt: "Hima Hair Salon" },
-  { src: img7032, alt: "Hima Hair Salon" },
-  { src: img7078, alt: "Hima Hair Salon" },
+  { src: img9576, thumb: thumb9576, alt: "Hima Hair Salon" },
+  { src: img0038, thumb: thumb0038, alt: "Hima Hair Salon" },
+  { src: img0682, thumb: thumb0682, alt: "Hima Hair Salon" },
+  { src: img0719, thumb: thumb0719, alt: "Hima Hair Salon" },
+  { src: img0730, thumb: thumb0730, alt: "Hima Hair Salon" },
+  { src: img0944, thumb: thumb0944, alt: "Hima Hair Salon" },
+  { src: img0950, thumb: thumb0950, alt: "Hima Hair Salon" },
+  { src: img0952, thumb: thumb0952, alt: "Hima Hair Salon" },
+  { src: img0955, thumb: thumb0955, alt: "Hima Hair Salon" },
+  { src: img2544, thumb: thumb2544, alt: "Hima Hair Salon" },
+  { src: img5074, thumb: thumb5074, alt: "Hima Hair Salon" },
+  { src: img5075, thumb: thumb5075, alt: "Hima Hair Salon" },
+  { src: img5129, thumb: thumb5129, alt: "Hima Hair Salon" },
+  { src: img5187, thumb: thumb5187, alt: "Hima Hair Salon" },
+  { src: img6599, thumb: thumb6599, alt: "Hima Hair Salon" },
+  { src: img7032, thumb: thumb7032, alt: "Hima Hair Salon" },
+  { src: img7078, thumb: thumb7078, alt: "Hima Hair Salon" },
 ];
 
 const Gallery = React.memo(() => {
@@ -72,10 +91,15 @@ const Gallery = React.memo(() => {
             elementClassNames="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4"
           >
             {galleryImages.map((image, index) => (
-              <a key={index} className="gallery-item" href={image.src}>
+              <a
+                key={index}
+                className="gallery-item"
+                href={image.src} // Original image shown in lightbox
+                data-lg-size="1600-2400" // Optional: Set the size of the lightbox
+              >
                 <img
                   alt={image.alt}
-                  src={image.src}
+                  src={image.thumb} // Thumbnail shown in grid
                   className="w-full aspect-square object-cover rounded-lg hover:opacity-90 transition-opacity"
                 />
               </a>
