a2e83b8435df6207092ae950983dd10a74e86727
marsudien <minhphuoc1304@gmail.com>
2025-09-14 08:41:31 +0700

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import ContactUs from "./Pages/ContactUs";
 import Home from "./Pages/Home/Home";
-import PriceList from "./Pages/PriceList";
+//import PriceList from "./Pages/PriceList";
 import Products from "./Pages/Products";
 import Gallery from "./Pages/Gallery";
 import Wrapper from "./Wrapper";
